573cf9bbd0e1686bfa37c4955918280fd64a9855
AlaminAB <alaminislam907@gmail.com>
2020-05-21 23:56:50 +0600

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,11 @@
 
     // jtoggle menu 
 
-    var menu = document.querySelector(".menu-icon");
-    var icon1 = document.querySelector(".first-icon");
-    var icon2 = document.querySelector(".secound-icon");
-    var icon3 = document.querySelector(".third-icon");
-
-    menu.addEventListener("click", function() {
-        icon2.classList.toggle("none");
-        icon1.classList.toggle("icon");
-        icon3.classList.toggle("icons");
+    $('.menu-icon').click( function() {
+        $('.secound-icon').toggleClass("none");
+       $('.first-icon').toggleClass("icon");
+        $('.third-icon').toggleClass("icons");
     })
       
+
+     
