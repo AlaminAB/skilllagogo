9a69624a824869015f2d5782a92109daf3e4cc51
AlaminAB <alaminislam907@gmail.com>
2020-05-21 23:26:16 +0600

    all file

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+    // jtoggle menu 
+
+    var menu = document.querySelector(".menu-icon");
+    var icon1 = document.querySelector(".first-icon");
+    var icon2 = document.querySelector(".secound-icon");
+    var icon3 = document.querySelector(".third-icon");
+
+    menu.addEventListener("click", function() {
+        icon2.classList.toggle("none");
+        icon1.classList.toggle("icon");
+        icon3.classList.toggle("icons");
+    })
+      $('form.serach-form input').click(function() {
+        $('form.serach-form button').css('left', '10px');
+        $(this).css('padding-left', '45px')
+    })
